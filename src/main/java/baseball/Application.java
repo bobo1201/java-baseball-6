27c8fc0706b84de474552c9bcc1680c19a087905
bobo1201 <121841746+bobo1201@users.noreply.github.com>
2023-10-31 00:26:35 +0900

    23/10/31 00:25 "feat(baseball) : 서로 다른 임의의 수 3개 생성 기능 구현"

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,9 @@
 package baseball;
 
+import baseball.domain.NumberGenerator;
+
+import java.util.List;
+
 // 객체 지향 프로그래밍
 // 1. 기능을 가지고 있는 클래스를 인스턴스화(=객체)한다.
 // 2. 필요한 기능을 (역할에 맞는) 인스턴스가 수행하게 한다.(의인화)
@@ -7,7 +11,8 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-
-
+        NumberGenerator generator = new NumberGenerator();
+        List<Integer> numbers = generator.createRandomNumber();
+        System.out.println(numbers);
     }
 }
